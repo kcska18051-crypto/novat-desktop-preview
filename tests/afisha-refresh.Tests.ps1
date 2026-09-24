447ec56ded4e468b13b037f18fcd6968b826a72f
kcska18051-crypto <kcska18051@gmail.com>
2026-09-24 11:47:55 +0300

    Fix sidebar icons and Culture RF logo

--- tests/afisha-refresh.Tests.ps1
+++ tests/afisha-refresh.Tests.ps1
@@ -9,6 +9,16 @@ if ($html -notmatch 'ВЕЛИКИЙ ТЕАТР ПОБЕДЫ') {
     $failures.Add('The sidebar slogan was not updated')
 }
 
+if ($html -notmatch 'class="sidebar-pictogram sidebar-pictogram--eye"' -or
+    $html -notmatch 'class="sidebar-pictogram sidebar-pictogram--vk"' -or
+    $html -notmatch 'class="sidebar-pictogram sidebar-pictogram--telegram"') {
+    $failures.Add('Sidebar pictograms must be rendered as clean inline SVG icons')
+}
+
+if ($html -notmatch 'class="culture-logo__name">КУЛЬТУРА\.</span>\s*<span class="culture-logo__rf">РФ</span>') {
+    $failures.Add('Culture.RF logo must expose separately colored text on a transparent background')
+}
+
 $desktopDates = [regex]::Matches($html, 'class="number-day desktop-inline"').Count
 $separateNumbers = [regex]::Matches($html, 'class="date-number"').Count
 if ($desktopDates -eq 0 -or $separateNumbers -ne $desktopDates) {
@@ -23,6 +33,10 @@ $requiredPatterns = @(
     '(?s)\.menu-btn\s*\{[^}]*border:\s*1px\s+solid\s+var\(--brand-yellow\)'
     '(?s)\.data-item\s*\{[^}]*border-top-width:\s*1px'
     '(?s)\.c-list-wrap\s*>\s*\.month\s*\{[^}]*position:\s*sticky[^}]*top:\s*0'
+    '(?s)\.sidebar-pictogram\s*\{[^}]*color:\s*var\(--brand-yellow\)'
+    '(?s)\.aside-part\s+\.button-special\s*\{[^}]*background-color:\s*transparent\s*!important'
+    '(?s)\.culture-logo__name\s*\{[^}]*color:\s*#fff'
+    '(?s)\.culture-logo__rf\s*\{[^}]*color:\s*#f00'
 )
 
 foreach ($pattern in $requiredPatterns) {
